067eeb2846c93d44ceb0d7075e42319588f42e63
HongKongdemon <idenimail33@gmail.com>
2018-09-27 10:33:14 +0400

    Added entry point

--- src/PO62/Suslov/wdad/Class.java
+++ src/PO62/Suslov/wdad/Class.java
@@ -1,6 +1,6 @@
 package PO62.Suslov.wdad;
 class Class {
     public static void main(String[] args) {
-        System.out.println("I`m Danila Suslov, and I`m not a monkey!");
+        System.out.println("I`m Danila Suslov, and I`m not a monkey!!");
     }
 }
